2f1f06b6c96c7f2d678967b5e4da61fe6029c01f
Mạnh <100762257+Qmanh1804@users.noreply.github.com>
2024-06-18 12:47:40 +0700

    firt game

---
+++ app/src/main/java/com/example/snakegame/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.snakegame.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val LightGreen = Color(0xFF8DA25A)
+val DarkGreen = Color(0xFF2D321D)
